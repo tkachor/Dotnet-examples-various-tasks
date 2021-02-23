e3bb15f30857e9c8282cb06a15e5a8cad45a7a6e
TKachor <tkachor@gmail.com>
2021-02-23 17:58:15 +0200

    Add Element ToArray with method CopyTo

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -6,7 +6,21 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initialization of a one-dimensional array
+            int[] integerArray1 = new int[5];
+            int[] integerArray2 = { 0, 1, 2, 3, 4 };
+            int[] integerArray3 = new int[] { 0, 1, 2, 3, 4 };
+            int[] integerArray4 = new int[5] { 0, 1, 2, 3, 4 };
+
+
         }
+
+        public static T[] AddElementToArray<T>(this T[] array, T item)
+        {
+            return result;
+        }
+
     }
+
+    
 }
