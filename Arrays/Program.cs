2e28db3c6af53e537019352dc02da9e768655352
TKachor <tkachor@gmail.com>
2021-02-23 16:23:06 +0200

    Add new project Arrays

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace Itvdn_dotnet
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-          
+            Console.WriteLine("Hello World!");
         }
     }
 }
