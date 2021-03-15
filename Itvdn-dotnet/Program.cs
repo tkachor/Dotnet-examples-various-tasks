6a535e82c2e019b3d7630647b9c35e300a066097
TKachor <tkachor@gmail.com>
2021-03-15 20:42:15 +0200

    Add task break the number into numbers it to array

--- Itvdn-dotnet/Program.cs
+++ Itvdn-dotnet/Program.cs
@@ -5,27 +5,9 @@ namespace Itvdn_dotnet
     
     class Program
     {
-        static bool IsPrimeNumber(int number)
-        {
-            bool result = true;
-
-            if (number > 1)
-            {
-                for (int i = 2; i < number; i++)
-                {
-                    if (number % i == 0)
-                    {
-                        result = false;
-                        break;
-                    }
-                }
-            }
-
-            return result;
-        }
         static void Main(string[] args) 
-        {
-            Console.WriteLine(IsPrimeNumber(22));
+        {          
+           
         }         
        
     }
