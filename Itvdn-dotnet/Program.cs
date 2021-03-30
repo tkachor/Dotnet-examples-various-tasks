77d9d016e90ff4110c1c5995d9da76565b09dfca
TKachor <tkachor@gmail.com>
2021-03-30 19:20:01 +0300

    Add new arithmetic task

--- Itvdn-dotnet/Program.cs
+++ Itvdn-dotnet/Program.cs
@@ -7,51 +7,9 @@ namespace Itvdn_dotnet
 {   
     
     class Program
-    {
-        //Array inversion with loop   
-        static int[] InversionIntegerArray(int[] array)
-        {
-            int length = array.Length;
-            int midle = length / 2;
-            int temp;
-            for (int i = 0; i < midle; i++)
-            {
-                temp = array[i];
-                array[i] = array[length - i - 1];
-                array[length - i - 1] = temp;
-            }
+    {        
 
-            return array;
-        }
-
-        //Array inversion with list
-        static int[] InversionIntegerArrayWithList(int[] array)
-        {
-            List<int> list = array.ToList();
-            list.Reverse();
-
-            return list.ToArray();
-        }
-
-        // Array Sort 
-        static int[] ArraySort(int[] array)
-        {
-            int temp = 0;
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = 0; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }                    
-                }
-            }
-
-            return array;
-        }
+        
 
         static void Main(string[] args) 
         {         
