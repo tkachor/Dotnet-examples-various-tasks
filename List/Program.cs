b784b0a49965f686723fbba4ad9b8a6c0589e6fa
TKachor <tkachor@gmail.com>
2021-05-17 20:58:24 +0300

    add method to find in list minimal number

--- List/Program.cs
+++ List/Program.cs
@@ -1,9 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace List
-{
+{  
+
     class Program
     {
+        static int LargestNumber(List<int> number)
+        {
+            int result = 0;
+
+            result = (from n in number select n).Max();
+
+            return result;
+        }
+
+        static int MinimaltNumber(List<int> number)
+        {
+            int result = 0;
+
+            result = (from n in number select n).Min();
+
+            return result;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
