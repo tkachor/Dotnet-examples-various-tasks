9b76982f7625e9c27f669be7e00483b3641089ae
TKachor <tkachor@gmail.com>
2021-05-30 18:14:44 +0300

    Add new tuple method

--- Enum/Program.cs
+++ Enum/Program.cs
@@ -2,8 +2,25 @@
 
 namespace Enum
 {
-    class Program
+
+    public enum HttpErrors
     {
+        Bad_Request = 400,
+        Unauthorized = 401,
+        Payment_Required = 402,
+        Forbidden = 403,
+        Not_Found = 404,
+        Method_Not_Allowed = 405
+    }
+    
+
+class Program
+    {
+
+
+        
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
---
+++ Tuple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tuple
+{
+    class Program
+    {
+
+        //Returning two or more values from a function
+        private static (int, int) GetValues()
+        {
+            var result = (1, 3);
+            return result;
+        }
+
+        //Return a tuple of two integer elements
+        private static (int sum, int count) GetNamedValues(int[] numbers)
+        {
+            var result = (sum: 0, count: 0);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result.sum += numbers[i];
+                result.count++;
+            }
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
