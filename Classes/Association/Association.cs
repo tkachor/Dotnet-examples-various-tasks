28a8bee79493530464ec158ac91c32e22b27fdd3
TKachor <tkachor@gmail.com>
2021-07-23 21:09:08 +0300

    add an example of the factory method

---
+++ Classes/Association/Association.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes.Association
+{
+    class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Fabric method");
+        }
+    }
+
+    class Factory
+    {
+        public Product FactoryMethod()
+        {
+            return new Product();
+        }
+    }
+
+    class Association
+    {
+        static void Main()
+        {
+            Factory factory = new Factory();
+
+            Product product = factory.FactoryMethod();
+
+        }
+    }
+}
