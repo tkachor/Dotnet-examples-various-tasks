e1d1949e3d42d221f4a3950e870bd0e6ab741dbd
TKachor <tkachor@gmail.com>
2021-08-18 19:25:15 +0300

    Add sample of insertion sort method

--- SortingMethods/Program.cs
+++ SortingMethods/Program.cs
@@ -25,6 +25,23 @@ namespace SortingMethods
             }           
         }
 
+        //Insertion Sort
+        static void InsertionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+
 
 
 
@@ -37,9 +54,10 @@ namespace SortingMethods
             {
                 array[i] = random.Next(arrauLength);
                 Console.WriteLine(array[i]);
-            }            
+            }
 
-            BubbleSortInteger(array);
+            InsertionSort(array);
+            //BubbleSortInteger(array);
 
             Console.WriteLine("-------------------");
 
