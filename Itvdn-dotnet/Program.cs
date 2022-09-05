a7cd00ddaabf81bb80bb83930237b73130398e51
TKachor <tkachor@gmail.com>
2022-09-05 17:04:57 +0300

    Add datatime simple function

--- Itvdn-dotnet/Program.cs
+++ Itvdn-dotnet/Program.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Numerics;
 
 namespace Itvdn_dotnet
-{   
-    
+{
+
+   
+
     class Program
     {
         static void Main()
-        {  
-            
+        {
+
+            string countDays(DateTime d)
+            {
+                DateTime today = DateTime.Now;
+
+                if ((d - today).Days == 0)
+                {
+                    return "Today is the day!";
+                }
+                else if ((d - today).Days < 0)
+                {
+                    return "The day is in the past!";
+                }
+                else(today.Day < d.Day)
+                {
+                    return $"{(d - today).Days} days";
+                }               
+            }
 
 
-
+            Console.WriteLine(countDays(new DateTime(2022, 09, 3)));
         }
     }
 }
