a56ef54dadc84544232bad9a63c7801cb221e401
TKachor <tkachor@gmail.com>
2022-10-06 14:13:25 +0300

    Add function FillMatrixWithRombhus

--- Itvdn-dotnet/Program.cs
+++ Itvdn-dotnet/Program.cs
@@ -12,17 +12,77 @@ using System.Threading.Tasks;
 
 namespace Itvdn_dotnet
 {
-    public static class LogAnalysis
-    {     
-     
-    }
+    
 
     class Program
-    { 
-        static void Main()
-        {      
+    {
 
-         
+       
+        public static void PrintRhombusInMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            int midSize = size/2;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i < midSize)
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j < midSize + i && j < midSize - i)
+                        {
+                            matrix[i, j] = 1;
+                        }
+                        else if (j > midSize + i && j > midSize - i)
+                        {
+                            matrix[i, j] = 2;
+                        }
+                        else
+                        {
+                            matrix[i, j] = 0;
+                        }
+                    }
+                }               
+                else
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (j <= midSize - (size - i) && j <= midSize + (size - i))
+                        {
+                            matrix[i, j] = 3;
+                        }
+                        else if (j >= midSize - (size - i) && j >= midSize + (size - i))
+                        {
+                            matrix[i, j] = 4;
+                        }
+                        else
+                        {
+                            matrix[i, j] = 0;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                }
+                Console.WriteLine();
+            }            
+        }       
+
+
+
+        static void Main()
+        {
+
+            PrintRhombusInMatrix(3);
+
+
+
 
         }
     }
